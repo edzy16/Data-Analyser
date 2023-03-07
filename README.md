c06473547ab159fcd8e10f632e291c288bf169e5
edzy16 <edwinsiby16@gmail.com>
2023-03-07 13:20:44 +0530

    readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Data-Analyser
+Data Analysis Tool
+
+This project is a web-based data analysis tool created using React and Vite. The purpose of this tool is to allow users to upload data files, analyze the data, and visualize it in a two-dimensional graph.
+Getting Started
+
+To get started, clone the repository and run the following commands:
+
+bash
+
+npm install
+npm run dev
+
+This will start the development server on http://localhost:3000.
+Usage
+Uploading Data
+
+To upload data, click the "Upload Data" button and select the file you wish to upload. Currently, only CSV files are supported.
+Data Table
+
+Once data is uploaded, it will be displayed in the Data Table component. This component allows users to view the data, sort it, and filter it.
+Data Analyzer
+
+The Data Analyzer component allows users to analyze the data by displaying various statistics about each variable. This includes the type of variable, insights, and the number of blank cells.
+Data Visualizer
+
+The Data Visualizer component displays the data in a two-dimensional graph. Users can select which variables to display on the X and Y axes using the dropdown menus.
+Contributing
+
+Contributions are always welcome! If you would like to contribute to this project, please open an issue or submit a pull request.
--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,19 @@
 
 
     function App() {
-      const sampleData = `Var Name,Type,Insight,Blank count
-      Var1,Categorical,Category count=5 A=23 B=33 C=13,5
-      Var2,Numeric,Min=12 Max=65 Average=43,2
-      Var3,Categorical,Category count=5 A=23 B=33 C=13,0
-      Var4,Numeric,Min=12 Max=65 Average=43,0
-      Var5,Categorical,Category count=5 A=23 B=33 C=13,0
-      Var6,Numeric,Min=12 Max=65 Average=43,0
-      Var7,Categorical,Category count=5 A=23 B=33 C=13,0
-      Var8,Numeric,Min=12 Max=65 Average=43,0
-      Var9, Categorical, Category count = 5 A = 23 B = 33 C = 13, 0`;
+      // const sampleData = `Var Name,Type,Insight,Blank count
+      // Var1,Categorical,Category count=5 A=23 B=33 C=13,5
+      // Var2,Numeric,Min=12 Max=65 Average=43,2
+      // Var3,Categorical,Category count=5 A=23 B=33 C=13,0
+      // Var4,Numeric,Min=12 Max=65 Average=43,0
+      // Var5,Categorical,Category count=5 A=23 B=33 C=13,0
+      // Var6,Numeric,Min=12 Max=65 Average=43,0
+      // Var7,Categorical,Category count=5 A=23 B=33 C=13,0
+      // Var8,Numeric,Min=12 Max=65 Average=43,0
+      // Var9, Categorical, Category count = 5 A = 23 B = 33 C = 13, 0`;
 
-      const rows = sampleData.split("\n").map((row) => row.split(","));
-      console.log(rows);
+      // const rows = sampleData.split("\n").map((row) => row.split(","));
+      // console.log(rows);
 
       return (
         <div className="app">
